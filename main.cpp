b0f9105fd34f91a84d977d63a1e3945725685d54
sherwin <whereaswhile@gmail.com>
2014-07-31 18:23:17 -0500

    hello git

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <time.h>
+#include "./solution/ClimbingStairs.h"
+using namespace std;
+
+int main(void)
+{
+	Solution s;
+	time_t t1, t2;
+
+	cout << "testing..." << endl;
+	time(&t1);
+	s.Test();
+	time(&t2);
+	double t=difftime(t2, t1);
+	cout << t << " seconds elasped." << endl;
+}
+
